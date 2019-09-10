dcc367d4d3d810ab06bf587f2773203b8e08356b
m <mleipper@hotmail.com>
2019-09-10 14:54:15 +0100

    Add project files.

---
+++ Aritmetic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aritmetic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
