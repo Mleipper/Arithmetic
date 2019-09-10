ae48640246206d94d5b10eba04e5981d65f8b94f
m <mleipper@hotmail.com>
2019-09-10 20:32:48 +0100

    added file reader to project

--- Aritmetic/Program.cs
+++ Aritmetic/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Arithmetic.Calc;
 using System;
+using System.IO;
+using System.Linq;
 
 namespace Aritmetic
 {
@@ -7,13 +9,31 @@ namespace Aritmetic
     {
         static void Main(string[] args)
         {
+            try
+            {
+                
 
-            double[] data = { 1, 2, 3 };
-            var calc = new Calculator(data);
-            Console.WriteLine(calc.getStatsPrintOut());
-            Console.WriteLine();
-            calc.printBininfo();
+                double Value;
+                double[] data2;
+                var values = File.ReadAllLines("SampleData.csv")
+                .SelectMany(a => a.Split(',')
+                .Select(str => double.TryParse(str, out Value) ? Value : 0));
 
+                data2 = values.ToArray();
+               
+
+                double[] data = { 1, 2, 3 };
+                
+                var calc = new Calculator(data2);
+                Console.WriteLine(calc.getStatsPrintOut());
+                Console.WriteLine();
+                calc.printBininfo();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be found");
+                Console.WriteLine(e.Message);
+            }
             Console.ReadKey();
         }
     }
